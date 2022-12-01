6529789068e9ef088bca667d24497b5974e64ad8
JeonHyeonsu <wjsgustn1236@naver.com>
2022-12-01 21:10:52 +0900

    블록생성 - 현수

---
+++ minerSL/Assets/Scripts/WorldMap.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMap : MonoBehaviour
+{
+    public int sizeX; //땅의 좌우크기
+    public int sizeY; //땅의 상하크기
+
+    public float terDetail;  //Mathf.PerlinNoise적용
+    public float terHeight;  //Mathf.PerlinNoise적용
+    int seed;                //Mathf.PerlinNoise적용
+
+
+
+    public GameObject[] blocks; //블록 저장
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seed = Random.Range(100000, 999999);
+        GenerateTerrain();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //땅 만들기 함수
+    void GenerateTerrain()
+    {
+
+        Vector2 pos;
+        pos = GameObject.FindGameObjectWithTag("Block").transform.position;
+
+        //땅의 좌우,상하 크기까지 반복하며 블록복사 배치 
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++)
+            {
+                GameObject block = Instantiate(blocks[0], new Vector3(pos.x + x, pos.y + y, 0), Quaternion.identity) as GameObject;
+                block.transform.SetParent(GameObject.FindGameObjectWithTag("Block").transform);
+            }
+        }
+    }
+}
